9989615bfb61e87e2df89d63ee598d2accbb33a3
Youssef Wadie <youssefwadie14@gmail.com>
2022-08-31 10:49:15 +0200

    normalize variables

--- src/main/java/com/github/youssefwadie/videoduration/core/TraversalDetails.java
+++ src/main/java/com/github/youssefwadie/videoduration/core/TraversalDetails.java
@@ -3,5 +3,7 @@ package com.github.youssefwadie.videoduration.core;
 import java.nio.file.Path;
 
 public record TraversalDetails(String[] filesAssociations, boolean verbose, Path startingPath, int depth) {
-
+    public TraversalDetails {
+        startingPath = startingPath.toAbsolutePath();
+    }
 }
